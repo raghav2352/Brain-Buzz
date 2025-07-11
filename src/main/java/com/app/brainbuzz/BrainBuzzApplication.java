ba0ac3abfde82a064111dc7a9d637f54dc0d088f
raghav <raghavpurohit19@gmail.com>
2025-07-11 22:03:43 +0530

    Initial commit : Spring Boot setup for BrainBuzz backend

---
+++ src/main/java/com/app/brainbuzz/BrainBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.app.brainbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainBuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/brainbuzz/BrainBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.brainbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
